c8f068faa8c4479e6c98d2fe344fd4dfef0f8c47
EC2 Default User <ec2-user@ip-172-31-45-166.ap-northeast-1.compute.internal>
2019-12-25 01:24:37 +0000

    first

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 # gem 'capybara'
 gem 'selenium-webdriver'
 gem 'chromedriver-helper'
+
+gem 'unicorn-worker-killer'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,49 +5,49 @@ class HomeController < ApplicationController
   
   def selen
       
-        require 'selenium-webdriver'
-        require 'net/http'
-        require 'uri'
-        
-        options = Selenium::WebDriver::Chrome::Options.new
-        options.add_argument('--headless')
-        driver = Selenium::WebDriver.for :chrome, options: options
-        
-        id = ''
-        password = ''
-        
-        @url = 'https://www.buyma.com/login/'
-        
-        
-        
-        driver.navigate.to @url
-        driver.manage.timeouts.implicit_wait = 100
-        
-        
-        driver.find_element(:name, 'txtLoginId').send_keys(id)
-        driver.find_element(:name, 'txtLoginPass').send_keys(password)
-        driver.find_element(:css, '#login_do').click
-
-        sleep(5)
-        logger.debug("================================= driver = #{driver.find_element(:css, "#wrapper > div.bmm-l-wrap.my-page > div.my-page__sidebar > div > div.my-page-profile > div.my-page-profile__btns > div:nth-child(1) > a > span").text}")
-        # 左上の出品するボタンクリック
-        driver.find_element(:css, "#wrapper > div.bmm-l-wrap.my-page > div.my-page__sidebar > div > div.my-page-profile > div.my-page-profile__btns > div:nth-child(1) > a > i").click
-        sleep(10)
-        logger.debug("=================================")
-        # 出品取得
-        @info = driver.find_element(:css, ".bmm-c-heading__ttl").text
-        logger.debug("================================= shuppin = #{@info}")
-
-        
-        
-        # driver.find_element(:css, "#wrapper > div.bmm-l-wrap.my-page > div.my-page__sidebar > div > div.my-page-menu > ul > li:nth-child(5) > a").click
-
-        # 商品画像文書取得
-        # @info = driver.find_element(:css, "#SellUI-react-component-26d08da7-ec35-423c-b0c6-f14272c477b8 > div > div > div > div.bmm-l-cnt__body.sell-body.is-btnbar-fixed > div > p.sell-info-bar__ttl").text
-        # logger.debug("================================= info = #{@info}")
-        driver.quit
-
+      require 'selenium-webdriver'
+      require 'net/http'
+      require 'uri'
       
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--headless')
+      driver = Selenium::WebDriver.for :chrome, options: options
+      
+      array = []
+      
+      # neowing-----------------------------------------------------------------------------
+      
+      # http://www.neowing.co.jp/ 親URL
+      driver.get 'http://www.neowing.co.jp/' #URLを開く
+      
+      # 検索コードを入力し、検索結果ページに移動
+      element = driver.find_element(:name, 'q')
+      element.send_keys('KSCL-3112')   #CD型番
+      logger.debug("=======検索フォームにキーワードを入力しました==========")
+      
+      # 検索ボタンを取得しサブミット
+      element = driver.find_element(:class, 'search-button')  #検索ボタン
+      element.submit
+      logger.debug("=======検索結果「#{driver.current_url}」へ移動しました==========")
+      
+      # 検索結果の第一要素をクリック
+      element = driver.find_element(:class, 'item-wrap').click
+      logger.debug("=======第一要素「#{driver.current_url}」へ移動しました==========")
+      
+      # 曲数分曲名を取得
+      driver.find_elements(:xpath, '//*[@id="content"]/div[10]/div/table/tbody/tr').each do |node|
+          array << [
+            node.find_element(:css, 'td.track-no').text,
+            node.find_element(:css, 'td.track-title').text
+            ]
+      end
+      
+      array.each do |i|
+        logger.debug("=======#{i[0]} #{i[1]}==========")
+      end
+      # neowing--------------------------------------------------------------------------------
+      
+      driver.quit
   end
       
 end
