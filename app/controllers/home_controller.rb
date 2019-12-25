6a5e4c07ae318acc92f62b0ee3958ee3524dc2ce
EC2 Default User <ec2-user@ip-172-31-45-166.ap-northeast-1.compute.internal>
2019-12-25 03:15:26 +0000

    neo comp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,40 +14,58 @@ class HomeController < ApplicationController
       driver = Selenium::WebDriver.for :chrome, options: options
       
       array = []
-      
+      model_numbers = ['KSCL-3112', 'UICZ-8205', 'VTCL-60466', 'QWCE-00654', 'VICL-65250', 'UICZ-8205']
+
       # neowing-----------------------------------------------------------------------------
-      
-      # http://www.neowing.co.jp/ 親URL
-      driver.get 'http://www.neowing.co.jp/' #URLを開く
-      
-      # 検索コードを入力し、検索結果ページに移動
-      element = driver.find_element(:name, 'q')
-      element.send_keys('KSCL-3112')   #CD型番
-      logger.debug("=======検索フォームにキーワードを入力しました==========")
-      
-      # 検索ボタンを取得しサブミット
-      element = driver.find_element(:class, 'search-button')  #検索ボタン
-      element.submit
-      logger.debug("=======検索結果「#{driver.current_url}」へ移動しました==========")
-      
-      # 検索結果の第一要素をクリック
-      element = driver.find_element(:class, 'item-wrap').click
-      logger.debug("=======第一要素「#{driver.current_url}」へ移動しました==========")
-      
-      # 曲数分曲名を取得
-      driver.find_elements(:xpath, '//*[@id="content"]/div[10]/div/table/tbody/tr').each do |node|
-          array << [
-            node.find_element(:css, 'td.track-no').text,
-            node.find_element(:css, 'td.track-title').text
-            ]
+      model_numbers.each do |model_number|
+        
+        # Neowing Top URL
+        driver.get 'http://www.neowing.co.jp/' #URLを開く
+        
+        # 検索コードを入力し、検索結果ページに移動
+        element = driver.find_element(:name, 'q')
+        element.send_keys(model_number)   #CD型番
+        # logger.debug("=======検索フォームにキーワードを入力しました==========")
+        
+        # 検索ボタンを取得しサブミット
+        element = driver.find_element(:class, 'search-button')  #検索ボタン
+        element.submit
+        # logger.debug("=======検索結果「#{driver.current_url}」へ移動しました==========")
+        
+        # 検索結果の第一要素をクリック
+        element = driver.find_element(:class, 'item-wrap').click
+        # logger.debug("=======第一要素「#{driver.current_url}」へ移動しました==========")
+        
+        # トラック名を取得
+        track_name = driver.find_element(:xpath, '//*[@id="content"]/div[2]/div[2]/div/h1/span').text
+        
+        # トラックNoと曲名を取得
+        driver.find_elements(:xpath, '//*[@class="tracklist"]/tbody/tr').each do |node|
+            array << [
+              node.find_element(:css, 'td.track-no').text,
+              node.find_element(:css, 'td.track-title').text,
+              track_name,
+              model_number,
+              'neo'
+              ]
+        end
+        
+
       end
-      
-      array.each do |i|
-        logger.debug("=======#{i[0]} #{i[1]}==========")
-      end
-      # neowing--------------------------------------------------------------------------------
       
       driver.quit
-  end
+      # neowing--------------------------------------------------------------------------------
       
+    logger.debug("==============================================")
+      array.each do |i|
+        logger.debug("#{i[0]}) #{i[1]} no:#{i[3]} soce:#{i[4]} ")
+      end
+    logger.debug("==============================================")
+  end
+  
+  def selen2
+      
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#top'
+  get 'home/selen2' => 'home#selen2'
   get 'home/selen' => 'home#selen'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
